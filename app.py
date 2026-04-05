f7bcb4a126f6f53a5cbd06bf50dc178008a7125f
nigamdivya00610-cpu <nigamdivya00610@gmail.com>
2026-04-06 04:01:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,6 @@ if run:
     # -----------------------------
     st.subheader("📉 Graphs")
 
-    fig1, ax1 = plt.subplots(figsize=(2,2))
     ax1.plot(V)
     ax1.set_title("Viewers")
     st.pyplot(fig1)
