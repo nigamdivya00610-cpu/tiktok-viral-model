a00f85b90643f8e0dc3d107fd1e09b0d303651eb
nigamdivya00610-cpu <nigamdivya00610@gmail.com>
2026-04-06 09:03:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import random
 # PAGE CONFIG
 # -----------------------------
 st.set_page_config(layout="wide")
-st.title("📊 TikTok Viral Spread Model (Mathematical + Network)")
+st.title("📊 TikTok Viral Spread Model)
 
 # -----------------------------
 # THEORY + FORMULAS
